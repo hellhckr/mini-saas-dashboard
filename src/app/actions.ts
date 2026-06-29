46e2fe678a23f4768b0870320aa2f1cea93f1642
AbhinayMU <Abhinaymunnikrishnan@gmail.com>
2026-06-30 00:45:17 +0200

    fix : compiler crashes during the production build

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,6 +1,7 @@
 "use server";
 
-import { revalidatePath, redirect } from "next/cache";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 import { createClient } from "@/utils/supabase/server";
 import { projectSchema, ProjectFormData } from "@/lib/schema";
 
